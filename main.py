ff9ea84f55131ba044a8a400acb60b8e5e191f63
JavierZam <javier_zam@yahoo.co.id>
2024-05-29 14:18:20 +0700

    Fix Minor

--- main.py
+++ main.py
@@ -42,43 +42,45 @@ def upload_file_to_gcs(user_id: str, image_bytes: bytes, filename: str):
 
     return blob.public_url
 #test
-def predict(image_bytes: bytes, user_id: str):
-    image = Image.open(BytesIO(image_bytes))
-    image = image.resize((128, 128))
-    img_array = img_to_array(image)
-    img_array = np.expand_dims(img_array, 0) 
+def predict(image_bytes: bytes, user_id: str, original_filename: str):
+    try:
+        image = Image.open(BytesIO(image_bytes))
+        image = image.resize((128, 128))
+        img_array = img_to_array(image)
+        img_array = np.expand_dims(img_array, 0) 
 
-    prediction = model.predict(img_array)
-    predicted_class = labels[np.argmax(prediction)]
-    confidence = np.max(prediction)
+        prediction = model.predict(img_array)
+        predicted_class = labels[np.argmax(prediction)]
+        confidence = np.max(prediction)
 
-    food_ref = db.collection('foods').document(predicted_class)
-    food = food_ref.get()
+        food_ref = db.collection('foods').document(predicted_class)
+        food = food_ref.get()
 
-    if food.exists:
-        food_data = food.to_dict()
-        # Add prediction to the 'foods-history' subcollection of the user
-        user_ref = db.collection('users').document(user_id)
-        food_history_doc_ref = user_ref.collection('foods-history').document()
-        # Update firestore food data with GCS image url
-        food_data['image_url'] = upload_file_to_gcs(user_id, image_bytes, food_history_doc_ref.id)
-        food_history_doc_ref.set(food_data)
+        if food.exists:
+            food_data = food.to_dict()
+            # Add result to foods-history subcollection
+            user_ref = db.collection('users').document(user_id)
+            food_history_doc_ref = user_ref.collection('foods-history').document()
+            food_data['image_user'] = upload_file_to_gcs(user_id, image_bytes, original_filename)
+            food_history_doc_ref.set(food_data)
 
-        return {"nama": food_data['nama'], 
-                "kalori": food_data['kalori'], 
-                "satuan": food_data['satuan'], 
-                "image_url": food_data['image_url']}
-    else:
-        return {"error": f"No food named {predicted_class} found in database"}
+            return {"nama": food_data['nama'], 
+                    "kalori": food_data['kalori'], 
+                    "satuan": food_data['satuan'],
+                    "image_db": food_data['image_db'],
+                    "image_user": food_data['image_user']}
+        else:
+            return {"error": f"{predicted_class} not found in database"}
+    except Exception as e:
+        return {"error": "The prediction is failed, please try again later or use different image"}
 
 @app.post("/predict/{user_id}")
-async def create_upload_file(user_id: str, file: UploadFile):
+async def upload_image(user_id: str, file: UploadFile):
     try:
         image_bytes = await file.read()
-        prediction = predict(image_bytes, user_id)
+        prediction = predict(image_bytes, user_id, file.filename)
         return prediction
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
-if __name__ == '__main6__':
-    uvicorn.run(app, port=8080, host="0.0.0.0")
+if __name__ == '__main__': uvicorn.run(app, port=8080, host="0.0.0.0")
